fdae41b54c217c45e43dcdd46f33ef45aed2b3f2
marchete <marchete@users.noreply.github.com>
2020-02-19 09:22:57 +0100

    Create CGFunge.php
    This is a PHP file, Codingame treats it a output text.

---
+++ Optimization/CGFunge/CGFunge.php
@@ -0,0 +1,16 @@
+15
+v   >v   >>  >0"EMIRP TON">D:E         
+>D3-:>D1-:^               ^C<          
+    v    >0"EMIRP">D:E                 
+    >DD2/2*-:^    ^C<                  
+    v-*3/3DD<                          
+ > S:v>0"EMIRP TON"     v              
+    >5^        >0"EMIRP">D:E           
+     >DD*2XD2X-:v       ^C<            
+               v<                      
+               >1XD2XD2X/2XD2X*2XD2X- v
+     ^  +6X1<                          
+           v:-X2DX2*X2+2DX2/+2X2DX2DX1:
+           >0"EMIRP TON">v"NOT PRIME"0<
+                         >D:E          
+                         ^C<           
